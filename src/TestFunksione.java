46519812074f422dda9d8a068d6ecc7f0445c2bf
arditsarja <(arditsarja@gmail.com)>
2019-04-15 08:50:54 +0200

    Korigjo Output

--- src/TestFunksione.java
+++ src/TestFunksione.java
@@ -2,11 +2,11 @@ public class TestFunksione {
 
 
     public static void thuajPershdendetje() {
-        System.out.println("Pershendetje Te");
+        System.out.println("Pershendetje");
     }
 
     public static void thuajMirupafshim() {
-        System.out.println("Pershendetje Te");
+        System.out.println("Mirupafshim");
     }
 
     public static void main(String[] args) {
